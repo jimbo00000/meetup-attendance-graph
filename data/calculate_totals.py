dae365529f90802e7360cb47d568dc1d445bb5a3
Jim S <SusinnoJ@geophysical.com>
2014-12-29 14:24:40 -0500

    Dump totals to json file.

--- data/calculate_totals.py
+++ data/calculate_totals.py
@@ -17,13 +17,13 @@ def unix_time_millis(dt):
 
 event_rsvps = defaultdict(int)
 event_counts = defaultdict(int)
-json_object = {}
+json_objects = []
 
 with open('groups.json') as group_json:
     group_data = json.load(group_json)
 
+# Accumulate totals by month
 for g in group_data[0]["groups"]:
-    #print(g)
     mfile = "meetup_history_" + g +".json"
     #print(mfile)
 
@@ -33,16 +33,24 @@ for g in group_data[0]["groups"]:
             msepoch = d['time']
             #print(msepoch, d['yes_rsvp_count'])
             x = datetime.date.fromtimestamp(msepoch/1000)
-            #print(x)
             monthdate = datetime.datetime(x.year, x.month, 1)
             #print(monthdate)
             event_rsvps[monthdate] += d['yes_rsvp_count']
             event_counts[monthdate] += 1
 
-
+# Assemble into a json object
 for k,v in sorted(event_rsvps.iteritems()):
-    print(k,v, event_counts[k])
+    #print(k,v, event_counts[k])
     millis = unix_time_millis(k)
     #print('%f' % millis)
     #print('{0:f}'.format(millis))
-    print(int(millis))
+    #print(int(millis))
+    jo = {}
+    jo['time'] = int(millis)
+    jo['yes_rsvp_count'] = v
+    #print(jo)
+    json_objects.append(jo)
+
+#print json_objects
+with open('meetup_history_total.json', 'w') as tots:
+    json.dump(json_objects, tots)
